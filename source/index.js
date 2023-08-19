d7d80da79483ce59d3df5a23f8552c2a827f0876
Bryce <100310267+invatorzen@users.noreply.github.com>
2023-08-19 18:28:21 -0400

    Init

---
+++ source/index.js
@@ -0,0 +1,15 @@
+const { Client } = require('discord.js');
+const { CommandKit } = require('commandkit');
+require('dotenv/config');
+
+const client = new Client({
+    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
+});
+
+new CommandKit({
+    client,
+    commandsPath: `${__dirname}/commands`,
+    eventsPath: `${__dirname}/events`,
+});
+
+client.login(process.env.TOKEN);
